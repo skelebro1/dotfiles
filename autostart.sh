b7a6d646308b841ff7fdaac477601d5dd371973e
skele <deepdey9999@gmail.com>
2023-07-28 21:49:49 +0530

    uploading dotfiles

---
+++ autostart.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+sxhkd -c $HOME/.config/bspwm/sxhkdrc &
+
+$HOME/.config/bspwm/polybar/launch.sh &
+
+dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+xsetroot -cursor_name left_ptr &
+
+feh --bg-fill /home/skele/Pictures/wall.jpeg &
+run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+run volumeicon &
+#killall komorebi &
+#run komorebi &
+picom --config $HOME/.config/bspwm/picom.conf &
---
+++ polybar/launch.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+# More info : https://github.com/jaagr/bspwm/polybar/wiki
+
+# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
+# awesome-terminal-fonts
+# Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
+# --log=error
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+desktop=$(echo $DESKTOP_SESSION)
+count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
+
+case $desktop in
+
+    bspwm|/usr/share/xsessions/bspwm)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/bspwm/polybar/config &
+      done
+    else
+    polybar --reload mainbar-bspwm -c ~/.config/bspwm/polybar/config &
+    fi
+    # second polybar at bottom
+    # if type "xrandr" > /dev/null; then
+    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #     MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/bspwm/polybar/config &
+    #   done
+    # else
+    # polybar --reload mainbar-bspwm-extra -c ~/.config/bspwm/polybar/config &
+    # fi
+    ;;
+
+esac
